a584c6c2b759cf048881ef6ce80975e7a743e9ea
Ron McCoy <rongoods@gmail.com>
2023-10-31 18:15:24 +0100

    incomplete handleSubmit

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,17 +1,28 @@
 export default function Form({ onAddActivity }) {
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+    onAddActivity({
+      name: data.name,
+      isForGoodWeather: data.isForGoodWeather.checked,
+    });
+    event.target.reset();
+    event.target.elements.name.focus();
+  }
   return (
     <>
       <h2>Add new Activity</h2>
-      <form className="container">
+      <form className="container" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name">Name:</label>
           <input name="name" id="name" type="text" />
         </div>
         <div>
-          <label htmlFor="isGoodWeatherActivity">Good-weather activity:</label>
+          <label htmlFor="isForGoodWeather">Good-weather activity:</label>
           <input
-            name="isGoodWeatherActivity"
-            id="isGoodWeatherActivity"
+            name="isForGoodWeather"
+            id="isForGoodWeather"
             type="checkbox"
           />
         </div>
