8ac5eacb945ea39ef3ebed5c19e92b648c782fa7
Ron McCoy <rongoods@gmail.com>
2023-11-01 14:31:06 +0100

    finish filter list

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,22 @@ export default function App() {
   const [activities, setActivities] = useLocalStorageState("items", {
     defaultValue: [],
   });
+
+  const isGoodWeather = true;
+
   function handleAddActivity(newActivity) {
     const id = uid();
-    setActivities([{ ...newActivity, id }, ...activities]);
+    setActivities(
+      [{ ...newActivity, id }, ...activities].filter(
+        (activity) => activity.isForGoodWeather == isGoodWeather
+      )
+    );
     // console.log([{ ...newActivity, id }, ...activities]);
     console.log(id);
   }
   return (
     <>
-      <List activities={activities}></List>
+      <List activities={activities} isGoodWeather={isGoodWeather}></List>
       <Form onAddActivity={handleAddActivity}></Form>
     </>
   );
--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,8 +1,17 @@
-export default function List({ activities }) {
+export default function List({ activities, isGoodWeather }) {
   // console.log(activities);
   const listActivities = activities.map((activity) => (
     <li key={activity.id}>{activity.name}</li>
   ));
 
-  return <ul>{listActivities}</ul>;
+  return (
+    <>
+      <h3>
+        {isGoodWeather
+          ? `The weather is awesome! Go outside and:`
+          : `Bad weather outside! Here's what you can do now:`}
+      </h3>
+      <ul>{listActivities}</ul>
+    </>
+  );
 }
