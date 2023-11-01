34286ea0e0fd339d44d9b9012381cb4e7f14dd4c
utkugunal <145130606+utkugunal@users.noreply.github.com>
2023-11-01 11:53:16 +0100

    add the listing feature

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,20 @@ import "./App.css";
 import Form from "./components/Form";
 import { useState } from "react";
 import { uid } from "uid";
+import List from "./components/List";
 
-function App() {
-  const [activities, setActivities] = useState({});
+export default function App() {
+  const [activities, setActivities] = useState([]);
   function handleAddActivity(newActivity) {
     const id = uid();
     setActivities([{ ...newActivity, id }, ...activities]);
+    // console.log([{ ...newActivity, id }, ...activities]);
+    console.log(id);
   }
   return (
     <>
+      <List activities={activities}></List>
       <Form onAddActivity={handleAddActivity}></Form>
     </>
   );
 }
-
-App();
-export default App;
